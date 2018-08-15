c98be495910341e935f1a65dff93e861f1bf7894
abhijit <abhijit@LAPTOP-SKS3RAE3>
2018-08-15 00:25:04 -0400

    second pritnln change

--- javagit/src/javagit/Main.java
+++ javagit/src/javagit/Main.java
@@ -11,5 +11,6 @@ public class Main {
 	private static void displayGreetings() {
 		System.out.println("TODO:Display Sales Data");
 		System.out.println("First Change");
+		System.out.println("Second Change");
 	}
 }
